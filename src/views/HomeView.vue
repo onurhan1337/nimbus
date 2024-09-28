3d05cad302fbbe232bd9fa85216354467021d396
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-09-28 17:57:35 +0300

    feat: add country list to home file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,35 @@
+<script setup lang="ts">
+import { onMounted, ref, toRefs } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const props = defineProps(['session'])
+const { session } = toRefs(props)
+
+interface Country {
+  id: number
+  name: string
+}
+
+const countries = ref<Country[]>([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = (data as Country[]) || []
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
 <template>
-  <main>
-    <h1>Welcome to the App</h1>
-  </main>
+  <div v-if="session">
+    <ul>
+      <li v-for="country in countries" :key="country.id">
+        {{ country.name }}
+      </li>
+      <button v-if="countries.length === 0" @click="getCountries">Add new country</button>
+    </ul>
+  </div>
+  <RouterLink to="/register" v-else />
 </template>
