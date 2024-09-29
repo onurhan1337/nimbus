469a2a01ca73582941e563a6451f6fc23f49b282
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-09-29 23:43:07 +0300

    fix: remove unnecessary part of home file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,5 @@
-<script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { supabase } from '../lib/supabaseClient'
-
-interface Country {
-  id: number
-  name: string
-}
-
-const countries = ref<Country[]>([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = (data as Country[]) || []
-}
-
-onMounted(() => {
-  getCountries()
-})
-</script>
-
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">
-      {{ country.name }}
-    </li>
-  </ul>
+  <section>
+    <h1>Home</h1>
+  </section>
 </template>
