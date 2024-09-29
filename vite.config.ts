2da76891993f9614e9349447558f5fb3a0078008
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-09-29 18:06:48 +0300

    feat: add radix vue package

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import RadixVueResolver from 'radix-vue/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      dts: true,
+      resolvers: [RadixVueResolver()]
+    })
   ],
   resolve: {
     alias: {
